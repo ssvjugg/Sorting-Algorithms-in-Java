f2d30236d19e7e9b60994376d606139a05115780
usernamedrew <59914219+ssvjugg@users.noreply.github.com>
2024-11-20 00:59:04 +0300

    junit5 added

---
+++ src/MainTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MainTest {
+    int[] arr;
+
+    @BeforeEach
+    public void randomArray() {
+        arr = new int[10];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = ThreadLocalRandom.current().nextInt(20);
+    }
+
+    @Test
+    public void testBubbleSort() {
+        Main.bubbleSort(arr);
+        
+    }
+}
